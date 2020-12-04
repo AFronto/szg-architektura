52e49fc537b6ee391b16f47b88c6888f3b24c445
Bödő Lajos <ifj.bodo.lajos@gmail.com>
2020-12-04 15:44:28 +0100

    Added questions to TopicData.ts

--- frontend/src/data/server/Topic/TopicData.ts
+++ frontend/src/data/server/Topic/TopicData.ts
@@ -1,8 +1,10 @@
 import UserData from "../User/UserData";
+import QuestionData from "./QuestionData";
 
 export default interface TopicData {
   id: string;
   name: string;
   description: string;
   owner: UserData;
+  questions: QuestionData[];
 }
