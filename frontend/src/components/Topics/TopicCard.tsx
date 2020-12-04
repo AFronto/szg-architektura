3c600a9b9b72ecd93147b66baf45215387c2b6f9
Frontó András <andrasfronto@gmail.com>
2020-12-04 21:21:36 +0100

    view topic button fix

--- frontend/src/components/Topics/TopicCard.tsx
+++ frontend/src/components/Topics/TopicCard.tsx
@@ -11,17 +11,8 @@ export const TopicCard: FunctionComponent<{ topic: TopicData }> = (props) => {
       <Card.Header as="h5">{props.topic.name}</Card.Header>
       <Card.Body>
         {props.topic.description}
-        <div
-          style={{
-            position: "fixed",
-            bottom: 0,
-            right: 0,
-            zIndex: 1,
-            marginBottom: 30,
-            marginRight: 30,
-          }}
-        >
-          <Button size="lg" variant="success" onClick={() => {}}>
+        <div className="d-flex justify-content-end">
+          <Button variant="success" onClick={() => {}}>
             View Topic
           </Button>
         </div>
